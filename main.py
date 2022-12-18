7d1e4eedce9714ec8fc919d5af168fe76d73edf9
sd369888 <sd369888@outlook.com>
2022-12-18 14:54:23 +0800

    Update Version Code

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import pyautogui
 import time
 from datetime import datetime
 import cv2
+'''
+V1.0
+'''
+
 def ScreenshotAndCV(tempFile, whatDo, debug=True):
     pyautogui.screenshot('./resource/big.png')
-    gray = cv2.imread("./resource/big.png",0)
-    img_template = cv2.imread(tempFile,0)
+    gray = cv2.imread("./resource/big.png", 0)
+    img_template = cv2.imread(tempFile, 0)
     w, h = img_template.shape[::-1]
-    res = cv2.matchTemplate(gray,img_template,cv2.TM_SQDIFF)
+    res = cv2.matchTemplate(gray, img_template, cv2.TM_SQDIFF)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     top = min_loc[0]
     left = min_loc[1]
@@ -20,27 +24,29 @@ def ScreenshotAndCV(tempFile, whatDo, debug=True):
     whatDo(x)
 
     if debug:
-        img = cv2.imread("./resource/big.png",1)
-        cv2.rectangle(img,top_left, bottom_right, (0,0,255), 2)
-        img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
-        cv2.imshow("processed",img)
+        img = cv2.imread("./resource/big.png", 1)
+        cv2.rectangle(img, top_left, bottom_right, (0, 0, 255), 2)
+        img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5,
+                         interpolation=cv2.INTER_NEAREST)
+        cv2.imshow("processed", img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     os.remove("./resource/big.png")
-    
+
+
 def signIn(meeting_id):
     '''
     Enter meeting NO. and join；
     '''
     # Start voov
     os.startfile("C:\Program Files (x86)\Tencent\WeMeet\wemeetapp.exe")
-    time.sleep(10)# wait for start
+    time.sleep(10)  # wait for start
     ScreenshotAndCV("./resource/joinbtn.png", pyautogui.click, False)
-    time.sleep(1)# get a screenshot to find buttons
+    time.sleep(1)  # get a screenshot to find buttons
     ScreenshotAndCV("./resource/meeting_id.png", pyautogui.click, False)
     pyautogui.write(meeting_id)
     ScreenshotAndCV("./resource/final.png", pyautogui.click, False)
-    if password_exsist==True:
+    if password_exsist == True:
         ScreenshotAndCV("./resource/passwordbox.png", pyautogui.click, False)
         time.sleep(5)
         pyautogui.write(password)
@@ -49,12 +55,13 @@ def signIn(meeting_id):
 
 
 while True:
-    now = datetime.now().strftime("%H:%M")#dont forget to add 0 before A.M. time!
-    password_exsist=True
-    debugging=True
-    password='666666'#meeting room password goes here
-    if now=="13:40" or debugging==True:#here goes your time!
-        meeting_id = '12345678'#Your meeting number goes here!
+    # dont forget to add 0 before A.M. time!
+    now = datetime.now().strftime("%H:%M")
+    password_exsist = True
+    debugging = True
+    password = '666666'  # meeting room password goes here
+    if now == "13:40" or debugging == True:  # here goes your time!
+        meeting_id = '12345678'  # Your meeting number goes here!
         time.sleep(5)
         signIn(meeting_id)
         print('Joined.')
